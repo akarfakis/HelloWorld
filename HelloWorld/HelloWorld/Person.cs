7f8191d38b2569e31b4a42ef6f114a83de21b07d
akarfakis.math@gmail.com <akarfakis.math@gmail.com>
2019-06-04 14:15:03 +0300

    Another Update

--- HelloWorld/HelloWorld/Person.cs
+++ HelloWorld/HelloWorld/Person.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+    public class Person
     {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("Hello World!");
-        }
+        public string Name { get; set; }
     }
 }
